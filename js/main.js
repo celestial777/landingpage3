47ad6ed5fe88b9451f19616eace664aaa39ea2bb
celestial777 <lovingmarvin@gmail.com>
2020-10-05 11:37:03 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+<script>
+  var MenuItems = document.getElementById("MenuItems");
+
+  MenuItems.style.maxHeight = "0px";
+  function menutoggle(){
+    if(MenuItems.style.maxHeight == "0px")
+        {
+          MenuItems.style.MaxHeight = "200px";
+        }
+        else
+          {
+            MenuItems.style.maxHeight == "0px";
+          }
+
+  }
+</script>
